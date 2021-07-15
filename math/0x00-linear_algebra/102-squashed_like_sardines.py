49e79d33f713ab9c8507ff3272fcf66aac62068a
Speedy171 <3212@holbertonschool.com>
2021-07-15 15:37:41 +0000

    Fixed 102

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -1,40 +1,33 @@
 #!/usr/bin/env python3
-"""
-Module that concatenates two matrices
-"""
-
-
-def matrix_shape(matrix):
-    """
-    Needs a matrix as input
-    Returns the shape as a list of integers
-    """
-    if type(matrix[0]) is not list:
-        return [len(matrix)]
-    else:
-        return [len(matrix)] + matrix_shape(matrix[0])
-
-
-def concat_recursive(mat1, mat2, axe):
-    """
-    Needs a matrix as input
-    Returns a concatenated matrix
-    """
-    result = []
-    if axe == 0:
-        result = mat1 + mat2
-        return result
-    for i in range(len(mat1)):
-        result.append(concat_recursive(mat1[i], mat2[i], axe - 1))
-    return result
+"""Functions concatenating two matrices along an axis"""
 
 
 def cat_matrices(mat1, mat2, axis=0):
-    """
-    Needs a matrix as input
-    Returns the resulting matrix
-    """
-    if len(matrix_shape(mat1)) > axis and len(matrix_shape(mat2)) > axis:
-        return concat_recursive(mat1, mat2, axis)
-    else:
-        return None
+    """Function that concatenates two matrices along a specific axis"""
+    shape1 = matrix_shape(mat1)
+    shape2 = matrix_shape(mat2)
+    del shape1[axis]
+    del shape2[axis]
+    return None if shape1 != shape2 else cat_matrices_comp(mat1, mat2, axis)
+
+
+def cat_matrices_comp(mat1, mat2, axis):
+    """Recursive function concatenating two matrices along a specific axis"""
+    if axis != 0:
+        return [cat_matrices_comp(u, v, axis - 1) for u, v in zip(mat1, mat2)]
+    return matrix_copy(mat1 + mat2)
+
+
+def matrix_copy(matrix):
+    """Function that returns a deep copy of the matrix"""
+    if len(matrix) != 0 and isinstance(matrix[0], list):
+        return list(map(matrix_copy, matrix))
+    return matrix[:]
+
+
+def matrix_shape(matrix):
+    """Function that returns the shape of a matrix"""
+    shape = [len(matrix)]
+    if len(matrix) != 0 and isinstance(matrix[0], list):
+        shape += matrix_shape(matrix[0])
+    return shape
